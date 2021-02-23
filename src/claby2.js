9e9985b95271999a12d6ec89da694485dad5cd3c
claby2 <wibow9770@gmail.com>
2021-02-23 16:21:41 +0800

    feat: add claby2 author card

---
+++ src/claby2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Box, Heading } from "@chakra-ui/react";
+
+export default class claby2 extends React.Component {
+    render() {
+        return (
+            <Box>
+                <Heading>This is a test</Heading>
+            </Box>
+        );
+    }
+}
